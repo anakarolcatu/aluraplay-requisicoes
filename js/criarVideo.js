e0704b83fd161d642321f7b3c3dc4f834f5b8f6b
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-05-03 17:12:23 -0300

    adicionando a funcao de cadastrar video

---
+++ js/criarVideo.js
@@ -0,0 +1,21 @@
+import { conectaApi } from "./conectaApi.js";
+
+const formulario = document.querySelector("[data-formulario]");
+
+async function criarVideo(evento) {
+    //previne o comportamento padrão do formulário
+    evento.preventDefault();
+
+    const imagem = document.querySelector("[data-imagem]").value;
+    const url = document.querySelector("[data-url]").value;
+    const titulo = document.querySelector("[data-titulo]").value;
+    const descricao = Math.floor(Math.random() * 10).toString();
+
+    await conectaApi.criaVideo(titulo, descricao, url, imagem);
+
+    //redireciona a página caso o envio seja efetuado com sucesso
+    window.location.href = "../pages/envio-concluido.html";
+}
+
+//o formulário só será enviado ao clicar no botão submit
+formulario.addEventListener("submit", evento => criarVideo(evento));
